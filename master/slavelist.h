9cdf61b34dc88e40bc02766d2af3623f6eba8d50
Jahrme Risner <jrisner@pugetsound.edu>
2018-02-28 10:49:39 -0800

    Easily toggle local/remote testing

--- master/slavelist.h
+++ master/slavelist.h
@@ -6,20 +6,22 @@
 #define DB_CAPSTONE_3_ADDR "10.250.94.72"
 #define HOME_ADDR "127.0.1.1"
 
-/*
-#define NUM_SLAVES 3
+#define LOCALTEST
 
-static char SLAVE_ADDR[NUM_SLAVES][32] = {
-    DB_CAPSTONE_1_ADDR,
-    DB_CAPSTONE_2_ADDR,
-    DB_CAPSTONE_3_ADDR
-};
-*/
+#ifdef LOCALTEST
+    #define NUM_SLAVES 1
 
-// TESTING
-#define NUM_SLAVES 1
+    static char SLAVE_ADDR[NUM_SLAVES][32] = {
+        HOME_ADDR
+    };
+#else
+    #define NUM_SLAVES 3
 
-static char SLAVE_ADDR[NUM_SLAVES][32] = {
-    HOME_ADDR
-};
+    static char SLAVE_ADDR[NUM_SLAVES][32] = {
+        DB_CAPSTONE_1_ADDR,
+        DB_CAPSTONE_2_ADDR,
+        DB_CAPSTONE_3_ADDR
+    };
+#endif
+
 #endif
