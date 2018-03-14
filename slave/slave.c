096fcd2114564b35d516b0482083ec000d8a9917
Sam Burdick <smburdick@pugetsound.edu>
2018-03-14 01:32:21 -0700

    cleanup

--- slave/slave.c
+++ slave/slave.c
@@ -17,12 +17,10 @@
 #include <unistd.h>
 
 query_result *get_vector(u_int vec_id) {
-	printf("in get vector\n");
     /* Turn vec_id into the filename "vec_id.dat" */
     int number_size = (vec_id == 0 ? 1 : (int) (log10(vec_id) + 1));
     int filename_size = number_size + 6; /* ".dat" */
     char filename[16];
-	printf("Getting Vector %u\n", vec_id);
     snprintf(filename, 16, "v_%u.dat", vec_id);
     /* Necessary Variables */
     FILE *fp = NULL;
@@ -40,20 +38,16 @@ query_result *get_vector(u_int vec_id) {
         vector_val = (u_int64_t *) malloc(sizeof(u_int64_t) * num_elts);
         char buf[32];
         while (fgets(buf, 32, fp) != NULL) {
-            printf("Buf = %s\n", buf);
             if (vector_len > num_elts) {
                 num_elts *= 2;
                 vector_val = (u_int64_t *) realloc(vector_val, num_elts * sizeof(u_int64_t));
             }
             vector_val[vector_len++] = (u_int64_t) strtol(buf, NULL, 10);
-		printf("Vector val in loop: %llu\n", vector_val[vector_len - 1]);
         }
-        //free(vector_val);
         fclose(fp);
     }
     query_result *vector = (query_result *) malloc(sizeof(query_result));
     vector->vector.vector_val = vector_val;
-    printf("Vector val = %llu, len = %d\n", vector_val[0], vector_len);
     vector->vector.vector_len = vector_len;
     vector->exit_code = exit_code;
     return vector;
@@ -65,9 +59,7 @@ query_result *rq_pipe_1_svc(rq_pipe_args query, struct svc_req *req)
     query_result *next_result = NULL;
 
     u_int exit_code = EXIT_SUCCESS;
-	printf("calling get vec\n");
     this_result = get_vector(query.vec_id);
-	printf("Got vector: %d\n", this_result == NULL);
     /* Something went wrong with reading the vector. */
     if (this_result->exit_code != EXIT_SUCCESS) {
         return this_result;
@@ -79,21 +71,16 @@ query_result *rq_pipe_1_svc(rq_pipe_args query, struct svc_req *req)
 
     /* Recursive Query */
     else {
-	rq_pipe_args a = *(query.next);
-	printf("entering recursive query\n");
-	
+
         char *host = query.machine_addr;
-	printf("got host\n");
         CLIENT *client;
         client = clnt_create(host,
             REMOTE_QUERY_PIPE, REMOTE_QUERY_PIPE_V1, "tcp");
-	printf("clnt is null:%d\n", client == NULL);
         if (client == NULL) {
             clnt_pcreateerror(host);
             exit_code = EXIT_FAILURE;
         }
         else {
-		printf("piping\n");
             next_result = rq_pipe_1_svc(*(query.next), client);
 
             if (next_result == NULL) {
@@ -114,18 +101,10 @@ query_result *rq_pipe_1_svc(rq_pipe_args query, struct svc_req *req)
     u_int64_t *result_val = (u_int64_t *) malloc(sizeof(u_int64_t) * this_result->vector.vector_len);
     u_int result_len = 0;
 
-    /*  TODO:
-     *  Return the vector, not the vector's ID.
-     *  Use WAH_OR and WAH_AND functions instead of bitwise.
-     */
     if (query.op == '|') {
-	printf("Vec id = %d\n", query.vec_id);
-        printf("Exec. %llu | %llu\n", this_result->vector.vector_val[0], next_result->vector.vector_val[0]);
-
         result_len = OR_WAH(result_val,
             this_result->vector.vector_val, this_result->vector.vector_len,
             next_result->vector.vector_val, next_result->vector.vector_len);
-	printf("Result len = %d\n", result_len);
     }
     else if (query.op == '&') {
         result_len = AND_WAH(result_val,
@@ -138,11 +117,10 @@ query_result *rq_pipe_1_svc(rq_pipe_args query, struct svc_req *req)
 
     query_result *vector = (query_result *) malloc(sizeof(query_result));
     vector->vector.vector_len = result_len;
-	printf("Result val = %llu\n", result_val[0]);
     vector->vector.vector_val = result_val;
     vector->exit_code = exit_code;
-	free(this_result);
-	free(next_result);
+    free(this_result);
+    free(next_result);
     return vector;
 }
 
@@ -153,7 +131,6 @@ typedef struct coord_thread_args {
 } coord_thread_args;
 
 void *init_coordinator_thread(void *coord_args) {
-    // TODO: get a query result
     coord_thread_args *args = (coord_thread_args *) coord_args;
 
     CLIENT *clnt = clnt_create(args->args->machine_addr, REMOTE_QUERY_PIPE, REMOTE_QUERY_PIPE_V1, "tcp");
@@ -174,7 +151,6 @@ void *init_coordinator_thread(void *coord_args) {
 
 query_result *rq_range_root_1_svc(rq_range_root_args query, struct svc_req *req)
 {
-    // TODO: spawn a thread for each range
     int num_threads = query.num_ranges;
     unsigned int *range_array = query.range_array.range_array_val;
     pthread_t tids[num_threads];
@@ -208,11 +184,10 @@ query_result *rq_range_root_1_svc(rq_range_root_args query, struct svc_req *req)
         // allocate the appropriate number of args
         pthread_create(&tids[i], NULL, init_coordinator_thread, (void *) thread_args);
     }
-	for(i = 0; i < num_threads; i++) pthread_join(tids[i], NULL);
+    for (i = 0; i < num_threads; i++) pthread_join(tids[i], NULL);
     // TODO AND all the results together
-    query_result *res = (query_result *) malloc(sizeof(query_result));
-//    return res;
-	return results[0]; // XXX: doing this way for now, too tired to and together
+    //query_result *res = (query_result *) malloc(sizeof(query_result));
+    return results[0];
 }
 
 #define TESTING_SLOW_PROC 0
@@ -230,7 +205,7 @@ int *commit_msg_1_svc(int message, struct svc_req *req)
         /* possible reasons: lack of memory, ... */
         result = VOTE_ABORT;
     }
-    /* make the process run slow, so that */
+    /* make the process run slow, to test failure */
     if (TESTING_SLOW_PROC) {
         sleep(TIME_TO_VOTE + 1);
     }
